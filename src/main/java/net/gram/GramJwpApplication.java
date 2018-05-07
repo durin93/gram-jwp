0e9ca01da2097340873e6c9161ebf6f38bf291e4
fur <fur@fur>
2018-05-07 21:57:18 +0900

    initial commit

---
+++ src/main/java/net/gram/GramJwpApplication.java
@@ -0,0 +1,12 @@
+package net.gram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GramJwpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GramJwpApplication.class, args);
+	}
+}
